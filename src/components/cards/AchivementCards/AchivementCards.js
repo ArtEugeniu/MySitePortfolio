2d35bbaa9d12aa1eb731e1f9db0e9628a9f38c5a
Пользователь <ArtEugeniu>
2025-03-28 15:43:33 +0200

    Add full-size certificate photos

--- src/components/cards/AchivementCards/AchivementCards.js
+++ src/components/cards/AchivementCards/AchivementCards.js
@@ -1,23 +1,38 @@
 import './AchivementCards.scss';
 import CertificateCardData from '../../../assets/data/certificateData.json';
+import { useState } from 'react';
 
 function AchivementCards() {
+  const [selectedImage, setSelectedImage] = useState(null);
   return (
-    <ul className="certificateCards">
-      {CertificateCardData.certificates.map((item, index) => {
+    <>
+      <ul className="certificateCards">
+        {CertificateCardData.certificates.map((item, index) => {
 
-        const image = require(`../../../assets/images/${item.imageURL}`);
+          const image = require(`../../../assets/images/${item.imageURL}`);
 
-        return <li className="certificateCards__item" key={item + index}>
-          <img src={image} alt="" className="certificateCards__image" />
-          <h2 className="certificateCards__title">{item.title}</h2>
-          <div className="certificateCards__description">
-            <span>Platform: {item.platform}</span>
-            <span>Status: {item.date}</span>
+          return (
+            <li className="certificateCards__item" key={item + index}>
+              <img src={image} alt="" className="certificateCards__image" onClick={() => setSelectedImage(image)} />
+              <h2 className="certificateCards__title">{item.title}</h2>
+              <div className="certificateCards__description">
+                <span>Platform: {item.platform}</span>
+                <span>Status: {item.date}</span>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
+      { 
+        selectedImage && (
+          <div className="modal-overlay" onClick={() => setSelectedImage(null)}>
+            <div className="modal-content">
+              <img src={selectedImage} alt="" className='modal-image'/>
+            </div>
           </div>
-        </li>
-      })}
-    </ul>
+        )
+      }
+    </>
   )
 }
 
